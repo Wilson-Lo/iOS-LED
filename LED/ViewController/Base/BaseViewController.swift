56a8d6b904aca88fc747ad4b7aa2515fac12117b
wilson lo <lowilson180@gmail.com>
2021-02-17 20:10:32 +0800

    1.Add UDP send & receive.

--- LED/ViewController/Base/BaseViewController.swift
+++ LED/ViewController/Base/BaseViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class BaseViewController: UIViewController {
     
+    var alert: UIAlertController!
+    let UDPKey = "qzy159pkn333rty2" //UDP AES key
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //Looks for single or multiple taps.
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
-        
+        self.alert = UIAlertController(title: nil, message: "Please wait ...", preferredStyle: .alert)
         //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
         //tap.cancelsTouchesInView = false
         view.addGestureRecognizer(tap)
@@ -28,3 +31,23 @@ class BaseViewController: UIViewController {
     
 }
 
+extension BaseViewController{
+    
+    //show please wait dialog
+    private func showLoading(){
+        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
+        loadingIndicator.hidesWhenStopped = true
+        loadingIndicator.style = UIActivityIndicatorView.Style.gray
+        loadingIndicator.startAnimating();
+        
+        alert.view.addSubview(loadingIndicator)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    //cancel please wait dialog
+    private func closeLoading(){
+        dismiss(animated: false, completion: nil)
+    }
+    
+    
+}
--- LED/ViewController/View/SettingsViewController.swift
+++ LED/ViewController/View/SettingsViewController.swift
@@ -7,9 +7,20 @@
 //
 
 import UIKit
+import Network
+import CryptoKit
+import CocoaAsyncSocket
+import CryptoSwift
+import RSSelectionMenu
+import Toast_Swift
+import SwiftSocket
 
-class SettingsViewController: BaseViewController {
-
+class SettingsViewController: BaseViewController , GCDAsyncUdpSocketDelegate{
+    
+    var queueUDP: DispatchQueue!
+    var udpSendSocket: UDPClient!
+    var udpReceiveSocket: GCDAsyncUdpSocket!
+    
     @IBOutlet weak var hostnameEditText: UITextField!
     @IBOutlet weak var hostnameApplyBt: UIButton!
     @IBOutlet weak var ipEditText: UITextField!
@@ -19,16 +30,104 @@ class SettingsViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("SettingsViewController-viewDidLoad")
+        self.queueUDP = DispatchQueue(label: "com.gomax.udp", qos: DispatchQoS.userInitiated)
         self.ipEditText.isUserInteractionEnabled = false
         let dictionary = Bundle.main.infoDictionary!
         let version = dictionary["CFBundleShortVersionString"] as! String
         self.appVerLabel.text = "APP ver. " + String(version)
     }
-
-
+    
     override func viewWillAppear(_ animated: Bool) {
         print("SettingsViewController-viewWillAppear")
+        self.udpReceiveSocket = GCDAsyncUdpSocket(delegate: self, delegateQueue: DispatchQueue.main)
+        queueUDP.async {
+            do {
+                self.udpReceiveSocket.setIPv4Enabled(true)
+                self.udpReceiveSocket.setIPv6Enabled(false)
+                try self.udpReceiveSocket.bind(toPort: 65088)
+                try self.udpReceiveSocket.beginReceiving()
+                print("socket create successful")
+            } catch let error {
+                print(error)
+            }
+        }
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.queueUDP.async {
+            self.udpSendSocket = UDPClient.init(address: "255.255.255.255", port: 5002)
+            self.udpSendSocket.enableBroadcast()
+        }
+    }
+    
+    
+    //****** UDP Listener - Start ******
+    private func udpSocket(sock: GCDAsyncUdpSocket!, didConnectToAddress address: NSData!) {
+        print("didConnectToAddress");
+    }
+    
+    private func udpSocket(sock: GCDAsyncUdpSocket!, didNotConnect error: NSError!) {
+        print("didNotConnect \(String(describing: error))")
+    }
+    
+    func udpSocket(_ sock: GCDAsyncUdpSocket, didSendDataWithTag tag: Int) {
+        print("didSendDataWithTag")
+    }
+    
+    private func udpSocket(sock: GCDAsyncUdpSocket!, didNotSendDataWithTag tag: Int, dueToError error: NSError!) {
+        print("didNotSendDataWithTag")
+    }
+    
+    func udpSocket(_ sock: GCDAsyncUdpSocket, didReceive data: Data, fromAddress address: Data, withFilterContext filterContext: Any?) {
+        print("receive")
+        
+        if(data != nil){
+            var deviceInfo = aesDecode(data: data)
+            if(deviceInfo.count > 0){
+                print(String(deviceInfo[27]) + "." + String(deviceInfo[28]) + "." + String(deviceInfo[29]) + "." + String(deviceInfo[30]))
+            }
+        }
+        
+    }
+    //****** UDP Listener - End ******
     
 }
 
+extension SettingsViewController{
+    
+    
+    //encode udp cmd
+    func aesEncodeUDPCmd() -> [Byte]{
+        var aes = [Byte]()
+        do {
+            
+            let key1:Array<Byte> = [0x00, 0x0b, 0x80, 0x00, 0x45, 0x54, 0x48, 0x5f, 0x52, 0x45, 0x51, 0x00, 0x00,0x00,0x00,0x00]
+            //use AES-128-ECB mode
+            aes = try AES(key: self.UDPKey.bytes, blockMode: ECB(), padding: .noPadding).encrypt(key1)
+        } catch {}
+        
+        return aes
+    }
+    
+    //deocode udp feedback
+    func aesDecode(data: Data) -> [Byte]{
+        var aes = [Byte]()
+        do {
+            
+            //use AES-128-ECB mode
+            aes = try AES(key: self.UDPKey.bytes, blockMode: ECB(), padding: .noPadding).decrypt(data.bytes)
+        } catch {}
+        
+        return aes
+    }
+    
+    
+    @IBAction func sendUDP(sender: UIButton) {
+        
+        self.queueUDP.async {
+            self.udpSendSocket.send(data: self.aesEncodeUDPCmd())
+        }
+    }
+    
+}
